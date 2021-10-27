4a950115a615c10e24beee64c770fc57b7c5db1c
Gabrielle Madden <49839049+gabriellemadden@users.noreply.github.com>
2021-10-27 16:45:36 -0400

    Use SQLAlchemy instead of vanilla sql

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,27 +1,9 @@
 import json
-
-from pathlib import Path
-
-from project.app import app, init_db
-
-
-def test_index():
-    tester = app.test_client()
-    response = tester.get("/", content_type="html/text")
-
-    assert response.status_code == 200
-    assert response.data == b"Hello, World!"
-
-
-def test_database():
-    init_db()
-    assert Path("flaskr.db").is_file()
-
 import pytest
-import os
+
 from pathlib import Path
 
-from project.app import app, init_db
+from project.app import app, db
 
 TEST_DB = "test.db"
 
@@ -31,10 +13,11 @@ def client():
     BASE_DIR = Path(__file__).resolve().parent.parent
     app.config["TESTING"] = True
     app.config["DATABASE"] = BASE_DIR.joinpath(TEST_DB)
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{BASE_DIR.joinpath(TEST_DB)}"
 
-    init_db() # setup
-    yield app.test_client() # tests run here
-    init_db() # teardown
+    db.create_all()  # setup
+    yield app.test_client()  # tests run here
+    db.drop_all()  # teardown
 
 
 def login(client, username, password):
